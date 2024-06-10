a1e43f11a947b08e16f77caa468e66ab141afa32
yasimdirshad <dirshadyasim@gmail.com>
2024-06-10 12:56:57 +0530

    some changes are made

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, response } from "express";
-import axios from "axios";
-
+import axios, { AxiosResponse } from "axios";
 
 const router = express.Router();
 
@@ -26,7 +25,7 @@ router.post("/verify-otp", async (req: Request, res: Response) => {
         headers: {
           Authorization: `Bearer ${req.headers.authorization}`,
         },
-        withCredentials:true
+        withCredentials: true,
       }
     );
     res.status(200).json(response.data);
@@ -37,54 +36,125 @@ router.post("/verify-otp", async (req: Request, res: Response) => {
 
 router.post("/login", async (req: Request, res: Response) => {
   try {
-    const response = await axios.post(
+    const response: AxiosResponse = await axios.post(
       "http://localhost:3001/auth/login",
-      req.body
+      req.body,
+      {
+        withCredentials: true,
+      }
     );
+
     if (response.data && response.data.error) {
       throw new Error(response.data.error);
     }
+
+    let cookies: string[] | undefined = response.headers["set-cookie"];
+    const accessTokenCookie = cookies?.find((cookie) =>
+      cookie.startsWith("access_token")
+    );
+
+    const refreshTokenCookie = cookies?.find((cookie) =>
+      cookie.startsWith("refresh_token")
+    );
+
+    const refreshToken = refreshTokenCookie
+      ? refreshTokenCookie.split("=")[1].split(";")[0]
+      : null;
+
+    const accessToken = accessTokenCookie
+      ? accessTokenCookie.split("=")[1].split(";")[0]
+      : null;
+
+    res.cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+    res.cookie("refresh_token", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
     res.status(200).json(response.data);
-  } catch (error:any) {
-    if(error.response){
-      res.status(400).json({error:error.response.data.error})
-    }else{
-      res.status(500).json({error:'unexpected error occured'})
+  } catch (error: any) {
+    if (error.response) {
+      res.status(400).json({ error: error.response.data.error });
+    } else {
+      res.status(500).json({ error: "unexpected error occurred" });
+    }
+  }
+});
+
+router.post("/logout", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post("http://localhost:3001/auth/logout", {
+      withCredentials: true,
+    });
+
+    if (response.data && response.data.error) {
+      throw new Error(response.data.error);
+    }
+
+    let cookies: string[] | undefined = response.headers["set-cookie"];
+    console.log("cookies in logout", cookies);
+    res.clearCookie('access_token')
+    res.clearCookie('refresh_token')
+    
+    res.status(200).json(response.data);
+  } catch (error: any) {
+    if (error.response) {
+      res.status(400).json({ error: error.response.data.error });
+    } else {
+      res.status(500).json({ error: "unexpected error occured" });
     }
   }
 });
 
 router.post("/google", async (req: Request, res: Response) => {
   try {
-
     const response = await axios.post("http://localhost:3001/auth/google", {});
     res.status(response.status).json(response.data);
   } catch (error: any) {
-    res.status(500).json({ message: "An error occurred during Google signup", error: error.message });
+    res.status(500).json({
+      message: "An error occurred during Google signup",
+      error: error.message,
+    });
   }
 });
 
-router.post('/forgot-password',async (req:Request,res:Response)=>{
+router.post("/forgot-password", async (req: Request, res: Response) => {
   try {
-    const response = await axios.post('http://localhost:3001/auth/forgot-password',req.body)
-    res.status(200).json(response.data)
-    
+    const response = await axios.post(
+      "http://localhost:3001/auth/forgot-password",
+      req.body
+    );
+    res.status(200).json(response.data);
   } catch (error) {
-    res.status(500).json({message:'An Error occured during forgot password'})
+    res
+      .status(500)
+      .json({ message: "An Error occured during forgot password" });
   }
-})
+});
 
-router.post('/reset-password',async (req:Request,res:Response)=>{
+router.post("/reset-password", async (req: Request, res: Response) => {
   try {
-    console.log('reset password reached in server',req.body)
-    const response = await axios.post('http://localhost:3001/auth/reset-password',req.body,{
-      withCredentials:true
-    })
-    console.log('responsedata',response.data)
-    res.status(200).json(response.data)
+    console.log("reset password reached in server", req.body);
+    const response = await axios.post(
+      "http://localhost:3001/auth/reset-password",
+      req.body,
+      {
+        withCredentials: true,
+      }
+    );
+    console.log("responsedata", response.data);
+    res.status(200).json(response.data);
   } catch (error) {
-    res.status(500).json({message:'An Error occured during forgot password'})
+    res
+      .status(500)
+      .json({ message: "An Error occured during forgot password" });
   }
-})
+});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,29 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import authRoutes from './routes/authRoutes'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/authRoutes";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import axios from "axios";
 
-const app = express()
-app.use(cookieParser())
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:true}))
-
+const app = express();
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: "http://localhost:5173",
   credentials: true,
 };
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  next();
+axios.defaults.withCredentials = true;
+
+// app.use((req, res, next) => {
+//   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+//   next();
+// });
+
+app.use("/auth", authRoutes);
+
+app.listen(4000, () => {
+  console.log("Api gateway running on port 4000");
 });
-
-app.use('/auth',authRoutes)
-
-app.listen(4000,()=>{
-  console.log('Api gateway running on port 4000')
-})
