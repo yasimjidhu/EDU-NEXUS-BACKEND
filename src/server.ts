cf223c26140285f83130e07797e7942c9865624b
yasimdirshad <dirshadyasim@gmail.com>
2024-07-10 13:18:53 +0530

    proxy modified

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,39 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import authRoutes from './routes/authRoutes'
-import cors from 'cors'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import axios from "axios";
+import proxy from 'express-http-proxy';
 
-const app = express()
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(cors())
+const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-  next();
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+axios.defaults.withCredentials = true;
+
+const authServiceProxy = proxy('http://localhost:3001');
+const userServiceProxy = proxy('http://localhost:3002');
+const notificationServiceProxy = proxy('http://localhost:3003');
+const chatServiceProxy = proxy('http://localhost:3006');
+const courseServiceProxy = proxy('http://localhost:3004');
+const paymentServiceProxy = proxy('http://localhost:3005');
+
+app.use('/auth', authServiceProxy);
+app.use('/user', userServiceProxy);
+app.use('/chat', chatServiceProxy);
+app.use('/course', courseServiceProxy);
+app.use('/payment', paymentServiceProxy);
+app.use('/notification', notificationServiceProxy);
+
+app.listen(4000, () => {
+  console.log("API Gateway running on port 4000");
 });
-
-app.use('/auth',authRoutes)
-
-app.listen(4000,()=>{
-  console.log('Api gateway running on port 4000')
-})
