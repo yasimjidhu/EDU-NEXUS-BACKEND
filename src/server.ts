c7c59340851f19ec026645fd928b5f4ba873fc5f
yasimdirshad <dirshadyasim@gmail.com>
2024-07-11 18:27:34 +0530

    proxy changed

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import axios from "axios";
+import express, { Application } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import proxy from 'express-http-proxy';
+import axios from 'axios';
 
-const app = express();
+const app: Application = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
@@ -16,24 +15,40 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 axios.defaults.withCredentials = true;
 
-const authServiceProxy = proxy('http://localhost:3001');
-const userServiceProxy = proxy('http://localhost:3002');
-const notificationServiceProxy = proxy('http://localhost:3003');
-const chatServiceProxy = proxy('http://localhost:3006');
-const courseServiceProxy = proxy('http://localhost:3004');
-const paymentServiceProxy = proxy('http://localhost:3005');
+// Define proxies
+const authProxy = proxy('http://localhost:3001', {
+  proxyReqPathResolver: (req) => `/auth${req.url}`,
+});
+const userProxy = proxy('http://localhost:3002', {
+  proxyReqPathResolver: (req) => `/user${req.url}`,
+});
+const notificationProxy = proxy('http://localhost:3003', {
+  proxyReqPathResolver: (req) => `/notification${req.url}`,
+});
+const courseProxy = proxy('http://localhost:3004', {
+  proxyReqPathResolver: (req) => `/course${req.url}`,
+});
+const paymentProxy = proxy('http://localhost:3005', {
+  proxyReqPathResolver: (req) => `/payment${req.url}`,
+});
+const chatProxy = proxy('http://localhost:3006', {
+  proxyReqPathResolver: (req) => `/chat${req.url}`,
+});
 
-app.use('/auth', authServiceProxy);
-app.use('/user', userServiceProxy);
-app.use('/chat', chatServiceProxy);
-app.use('/course', courseServiceProxy);
-app.use('/payment', paymentServiceProxy);
-app.use('/notification', notificationServiceProxy);
 
-app.listen(4000, () => {
-  console.log("API Gateway running on port 4000");
+// Proxy middleware
+app.use('/auth', authProxy);
+app.use('/user', userProxy);
+app.use('/course', courseProxy);
+app.use('/payment', paymentProxy);
+app.use('/chat', chatProxy);
+app.use('/notification', notificationProxy);
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`API gateway running on port ${PORT}`);
 });
